a1dfa96b41cafedf6c72f722cb41464d823de5f7
innovata <innovata@users.noreply.github.com>
2023-03-26 17:51:58 +0900

    0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="innomongo",
-    version="0.0.0",
+    version="0.1.0",
     author="innovata sambong",
     author_email="iinnovata@gmail.com",
     description='pymongo 패키지 wrapper',
