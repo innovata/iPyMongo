290f9ddc76187ad6d302525b89f3b021c6968de1
innovata <innovata@users.noreply.github.com>
2023-08-11 17:17:31 +0900

    0.1.2

--- src/test/utest.py
+++ src/test/utest.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-sys.path.append(os.path.join('C:\\', 'pypjts', 'PyMongoDB', 'src'))
+sys.path.append(os.path.join('C:\\', 'pypjts', 'iPyMongo', 'src'))
 
-
-import innomongo
-innomongo.NAME = 'PyMongoDB'
-print(innomongo.Client['PyMongoDB'])
-m = innomongo.Collection('TestTable')
-print(m)
-print(m.name)
